5528c0b647eac915ba9875746b240e31adcf91d5
Carlos Fernandez <carlos.ferlo@outlook.com>
2026-02-17 12:39:04 +0100

    solve error of backward compatibility

--- python/implica/__init__.py
+++ python/implica/__init__.py
@@ -1,5 +1,7 @@
+from typing import Union
+
 from .implica import Graph, Query, Edge, Node, Term, Type, Constant
 
-type Element = Edge | Node | Term | Type
+Element = Union[Edge, Node, Term, Type]
 
 __all__ = ["Graph", "Query", "Edge", "Node", "Term", "Type", "Element", "Constant"]
