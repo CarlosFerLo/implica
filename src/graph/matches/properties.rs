64961d03cb6268d6839a88c8618156455088532e
Carlos Fernandez <carlos.ferlo@outlook.com>
2026-03-22 16:12:02 +0100

    solve inconsistency on check property matches

--- src/graph/matches/properties.rs
+++ src/graph/matches/properties.rs
@@ -49,7 +49,7 @@ impl Graph {
                 {
                     Ok(compare_values(value, &other))
                 } else {
-                    Ok(true)
+                    Ok(false)
                 }
             })
         } else {
